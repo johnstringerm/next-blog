bcd5915b66d01588304e35678e83760e116d7849
JohnSM <john.stringer.martin@gmail.com>
2020-06-08 09:01:11 +0100

    add pathmap to config

--- next.config.js
+++ next.config.js
@@ -19,5 +19,16 @@ module.exports = withFonts({
   env: {
     API_URL: process.env.API_URL,
   },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/resume": { page: "/resume" },
+      "/blog": { page: "/blog" },
+      "/projects": { page: "/projects" },
+    };
+  },
   // withFonts();
 });
