2ba9ba34294050e253a26887d6c578071e5bee2a
John Stringer <John Stringer>
2021-02-22 20:20:45 +0000

    UPDATE - Updating some text and fixing typo

--- pages/resume.js
+++ pages/resume.js
@@ -17,13 +17,26 @@ const Resume = () => {
         <div className="about-container">
           <h1>Resume</h1>
           <ResumeStyled>
+            <div className="title">Experience</div>
+            <ResumeItem
+              name="The Range"
+              role="Junior Software Developer"
+              duration="October 2020 - Present"
+              description="After graduating university, I've started working at The Range - a multi-channel retailer as a Junior Software Developer working on internal applications."
+            />
+            <ResumeItem
+              name="Formell Ltd."
+              role="Junior Developer"
+              duration="September 2016 - June 2017"
+              description="During my A Levels I worked part time for a business software solutions company in Roborough."
+            />
+
             <div className="title">Education</div>
             <ResumeItem
               name="University of Plymouth"
               role="Internet Design"
               duration="September 2017- May 2020"
-              description="A course centered around designing web based applications such as websites and mobile applications from design to programming.
-"
+              description="A course centered around designing web based applications such as websites and mobile applications from design to programming."
             />
             <ResumeItem
               name="Ivybridge Community College"
@@ -31,22 +44,17 @@ const Resume = () => {
               duration="September 2011 - June 2016"
               description="I studied ICT, Computing and Spanish during my A Level studies."
             />
-            <div className="title">Experience</div>
-            <ResumeItem
-              name="Formell Ltd."
-              role="Junior Developer"
-              duration="September 2016 - Jun 2017"
-              description="During my A Levels I worked part time for a business software solutions company in Roborough."
-            />
+
             <div className="title">Skills</div>
             <div className="text-content">
-              HTML • CSS • JavaScript • Git • NPM/Yarn • Photshop • Illustrator
-              • After Effects • React • Figma
+              JavaScript • React • React Native • Git • T-SQL • MySQL •
+              Microsoft SQL Server • C# • ASP.NET • Photoshop • Illustrator •
+              After Effects • Figma • HTML • CSS
             </div>
             <div className="title">Languages</div>
             <div className="text-content">
-              I can speak fluent English and Spanish, coming from a bilingual
-              English and Spanish household.
+              I can speak fluent English and Spanish, originally growing up in
+              Spain and relocating to the UK as child.
             </div>
           </ResumeStyled>
         </div>
